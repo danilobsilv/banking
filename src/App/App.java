b42c2da1b7671b583ed410e9ab98a929434a47c0
Danilo Bruno <danilobsilv@gmail.com>
2023-09-27 17:48:17 -0400

    testing methods

--- src/App/App.java
+++ src/App/App.java
@@ -1,7 +1,21 @@
 package src.App;
 
+import src.Database.DBConnection;
+import src.Database.CreateTables;
+
+import src.Controllers.UserController;
+import src.Models.User;
+
+import java.sql.SQLException;
+
 public class App {
-    public static void main(String[] args){
-        System.out.println("Hello, world!");
+    public static void main(String[] args) throws SQLException {
+        UserController cont = new UserController();
+        //User user = new User("usariobacana", "userbacana@gmail.com", "user123");
+        //cont.createUser(user);
+        cont.showUsers();
+        //cont.editUserUsername(3,"vai tomar no cu");
+        //cont.editUserPassword(3, "filho da puta");
+        //cont.deleteUser(3);
     }
 }
