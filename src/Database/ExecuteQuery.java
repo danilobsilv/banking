e884c4265a0dfe07021639d001bb4f6063525de0
Danilo Bruno <danilobsilv@gmail.com>
2023-09-27 17:46:00 -0400

    turned dbpath into local var

--- src/Database/ExecuteQuery.java
+++ src/Database/ExecuteQuery.java
@@ -4,13 +4,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class ExecuteQuery{
-
-    private final String dbPath = "bankApp.db";
-
+public class ExecuteQuery {
     public void executeQuery(String query, Object... params) throws SQLException {
+        final String dbPath = "bankApp.db";
         try (Connection connection = DBConnection.getConnection(dbPath);
-             PreparedStatement preparedStatement = connection.prepareStatement(query)){
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
             for (int i = 0; i < params.length; i++) {
                 preparedStatement.setObject(i + 1, params[i]);
             }
