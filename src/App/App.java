f111f56e663dfba2cdfdfcc6e2cf854da9d4c7cb
Danilo Bruno <danilobsilv@gmail.com>
2023-10-01 11:46:27 -0400

    todo]

--- src/App/App.java
+++ src/App/App.java
@@ -10,12 +10,6 @@ import java.sql.SQLException;
 
 public class App {
     public static void main(String[] args) throws SQLException {
-        UserController cont = new UserController();
-        //User user = new User("usariobacana", "userbacana@gmail.com", "user123");
-        //cont.createUser(user);
-        cont.showUsers();
-        //cont.editUserUsername(3,"vai tomar no cu");
-        //cont.editUserPassword(3, "filho da puta");
-        //cont.deleteUser(3);
+        //todo
     }
 }
