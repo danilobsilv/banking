1c012fd93613e11e8438e5e23a79ce872d946174
Danilo Bruno <danilobsilv@gmail.com>
2023-10-01 11:43:07 -0400

    add: functions to verify the existence of user's email and password

--- src/Database/ExecuteQuery.java
+++ src/Database/ExecuteQuery.java
@@ -1,7 +1,9 @@
 package src.Database;
 
+import javax.xml.transform.Result;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class ExecuteQuery {
@@ -14,5 +16,37 @@ public class ExecuteQuery {
             }
             preparedStatement.executeUpdate();
         }
+    }
+
+    public boolean emailExists(String dbPath, String query, String email) throws SQLException {
+        int count = 0;
+
+        try (Connection connection = DBConnection.getConnection(dbPath);
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, email);
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    count = ((ResultSet) resultSet).getInt(1);
+                }
+            }
+        }
+        return count > 0;
+    }
+
+    public boolean passwordExists(String dbPath, String query, String password) throws SQLException {
+        int count = 0;
+
+        try (Connection connection = DBConnection.getConnection(dbPath);
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, password);
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    count = ((ResultSet) resultSet).getInt(1);
+                }
+            }
+        }
+        return count > 0;
     }
 }
