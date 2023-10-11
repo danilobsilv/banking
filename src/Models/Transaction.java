4acf76f609f4278b7abd969da075b92d7b1bac7e
Danilo Bruno <danilobsilv@gmail.com>
2023-10-10 21:30:31 -0400

    add: toString

--- src/Models/Transaction.java
+++ src/Models/Transaction.java
@@ -1,52 +1,32 @@
 package src.Models;
 
+import src.enumerate.TransactionType;
 
 import java.util.Date;
 
 public class Transaction {
+    private float amount;
+    private TransactionType transactionType;
 
-    float amount;
-    String transactionType;
-    Integer accountId;
-    Date transactionDate;
-
-    public Transaction(String transactionId, float amount, String transactionType, Integer accountId,
-                       Date transactionDate) {
+    public Transaction(float amount, TransactionType transactionType){
         this.amount = amount;
         this.transactionType = transactionType;
-        this.accountId = accountId;
-        this.transactionDate = transactionDate;
     }
 
     public float getAmount() {
-        return amount;
+        return this.amount;
     }
 
-    public void setAmount(float amount) {
-        this.amount = amount;
+    public TransactionType getTransactionType() {
+        return this.transactionType;
     }
 
-    public String getTransactionType() {
-        return transactionType;
-    }
-
-    public void setTransactionType(String transactionType) {
-        this.transactionType = transactionType;
-    }
-
-    public Integer getAccountId() {
-        return accountId;
-    }
-
-    public void setAccountId(Integer accountId) {
-        this.accountId = accountId;
-    }
-
-    public Date getTransactionDate() {
-        return transactionDate;
-    }
-
-    public void setTransactionDate(Date transactionDate) {
-        this.transactionDate = transactionDate;
+    @Override
+    public String toString() {
+        return "Transaction { " +
+                "amount=" + amount +
+                ", transactionType=" + transactionType +
+                " }";
     }
 }
+
