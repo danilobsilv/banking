522c6ef234a74484a14811c1eaa5d13fb82f6149
Danilo Bruno <danilobsilv@gmail.com>
2023-10-18 23:00:46 -0400

    static class to read properties

--- src/env/ConfigReader.java
+++ src/env/ConfigReader.java
@@ -4,7 +4,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
-public class ConfigurationReader {
+public class ConfigReader {
     public static Properties loadProperties() throws IOException {
         Properties properties = new Properties();
         FileInputStream fileInputStream = new FileInputStream("src/env/.env");
