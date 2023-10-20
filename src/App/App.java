06fcc40849b8074ab94e7ce67920d2785fd40f2d
Danilo Bruno <danilobsilv@gmail.com>
2023-10-19 21:08:43 -0400

    tests

--- src/App/App.java
+++ src/App/App.java
@@ -1,22 +1,10 @@
 package src.App;
 
-import src.Controllers.BankAccountController;
-import src.Controllers.TransactionController;
-import src.Controllers.UserController;
+import src.Database.BankReports;
 
-import src.Database.ExecuteQuery;
-
-import src.Models.BankAccount;
-import src.Models.Transaction;
-import src.Models.User;
-
-import src.enumerate.AccountType;
-import src.enumerate.TransactionType;
-
-import java.util.Date;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        // todo
     }
 }
+
