e2cc9cf7a421f98bb3b88fbb8411fe4493baaba3
Danilo Bruno <danilobsilv@gmail.com>
2023-10-21 14:20:18 -0400

    add: mutual fund type

--- src/enumerate/InvestmentTypeNames.java
+++ src/enumerate/InvestmentTypeNames.java
@@ -2,9 +2,14 @@ package src.enumerate;
 
 public enum InvestmentTypeNames {
     CDB,
-    LCI,
-    LCA,
-    InvestmentTypeNames,
-    DirectTreasury
 
+    LCI,
+
+    LCA,
+
+    InvestmentTypeNames,
+
+    DirectTreasury,
+
+    MutualFund
 }
